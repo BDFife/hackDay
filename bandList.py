eb152e84bf6864efc06e56b4b838e51b07d7fb6c
BDFife <bdfife@gmail.com>
2010-10-17 11:59:11 -0400

    Modified bandlist to include a proper function that can be imported.

--- bandList.py
+++ bandList.py
@@ -11,39 +11,51 @@ Saves a json file bandlist.json that contains the ranked list of favourite bands
 import urllib
 import json
 
-##
-## Grab Last.FM data
-##
+def getTopArtistsFromID(lfmID):
+    """
+    Grab last.FM top artists when provided with a last.fm ID. 
+    
+    Returns a json-friendly dictionary that contains artist name associated with 'rank' and mbID. 
+    """
+    
+    lfmAPI = u'dbc675ab62528258254f6a6164074a55'
+    period = u'12month'
+    user = lfmID
+    
+    myURL='http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=%s&api_key=%s&period=%s&format=json' % (user, lfmAPI, period)
+    
+    data = urllib.urlopen(myURL).read()
+    data = json.loads(data)
+    
+    # higherarchy of last.fm data is:
+    # topartists
+    #   artist (list of artists)
+    #     name  (artist name)
+    #     playcount
+    #     mbid (musicbrainz ID)
+    #     url
+    #     streamable
+    #     image
+    #       <images by size>
+    #     @attr
+    #       rank (rank in top artist list)
+    
+    artists = {}
+    
+    # fixme: consider if this should be a list rather than a dict, with rank implied. 
+    # dict keys are listed as unicode strings, so preserving this.
+    for artist in data[u'topartists'][u'artist']:
+        artists[artist[u'name']]= {u'mbid':artist[u'mbid'], u'rank':int(artist[u'@attr']['rank'])} 
+    
+    # here's a nice breakpoint where JSON can be exported
+    
+    return artists
 
-# fixme: hardcoded last.fm information! 
-myURL='http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=jimmytheleaf&api_key=dbc675ab62528258254f6a6164074a55&period=12month&format=json'
-
-data = urllib.urlopen(myURL).read()
-data = json.loads(data)
-
-# higherarchy of last.fm data is:
-# topartists
-#   artist (list of artists)
-#     name  (artist name)
-#     playcount
-#     mbid (musicbrainz ID)
-#     url
-#     streamable
-#     image
-#       <images by size>
-#     @attr
-#       rank (rank in top artist list)
-
-artists = {}
-
-# fixme: consider if this should be a list rather than a dict, with rank implied. 
-# dict keys are listed as unicode strings, so preserving this.
-for artist in data[u'topartists'][u'artist']:
-    artists[artist[u'name']]= {u'mbid':artist[u'mbid'], u'rank':int(artist[u'@attr']['rank'])} 
-
-# here's a nice breakkpoint where JSON can be exported
-
-f = open("bandlist.json", "w")
-bandList = json.dump(artists, f, indent=4)
-f.close()
+if __name__ == "__main__":
+    
+    topArtists = getTopArtistsFromID('jimmytheleaf')
+    
+    f = open("bandlist2.json", "w")
+    bandList = json.dump(topArtists, f, indent=4)
+    f.close()
 
