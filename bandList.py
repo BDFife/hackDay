c27f5628f9bdcd08a18246001161154953d9e3bc
BDFife <bdfife@gmail.com>
2010-10-16 22:11:59 -0400

    minor changes to comments, etc.

--- bandList.py
+++ bandList.py
@@ -1,18 +1,25 @@
 """
-ConcertList.py
+bandList.py
 Jim Fingal and Brian Fife, for Music Hack Day Boston ][
 
 Take a user's last.fm data and search for upcoming concerts in their location.
+Currently hard-coded to Jim's Last.FM account.
+
+Saves a json file bandlist.json that contains the ranked list of favourite bands.
 """
 
 import urllib
 import json
 
+##
+## Grab Last.FM data
+##
+
 # fixme: hardcoded last.fm information! 
 myURL='http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=jimmytheleaf&api_key=dbc675ab62528258254f6a6164074a55&period=12month&format=json'
 
 data = urllib.urlopen(myURL).read()
-json = json.loads(data)
+data = json.loads(data)
 
 # higherarchy of last.fm data is:
 # topartists
@@ -31,9 +38,12 @@ artists = {}
 
 # fixme: consider if this should be a list rather than a dict, with rank implied. 
 # dict keys are listed as unicode strings, so preserving this.
-for artist in json[u'topartists'][u'artist']:
+for artist in data[u'topartists'][u'artist']:
+    artists[artist[u'name']]= {u'mbid':artist[u'mbid'], u'rank':int(artist[u'@attr']['rank'])} 
 
-    artists[artist[u'name']]= {'mbid':artist[u'mbid'], 'rank':int(artist[u'@attr']['rank'])} 
+# here's a nice breakkpoint where JSON can be exported
 
-print artists
+f = open("bandlist.json", "w")
+bandList = json.dump(artists, f, indent=4)
+f.close()
 
