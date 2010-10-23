ea0f44622597e9cba3f2bd52e55e155468f40f40
Brian Fife <brian@bluesock.org>
2010-10-22 23:09:05 -0700

    Updated templates to render full thing. Still looks much worse than the original htmlresponse version.
    The application is running very slow. I tried to run profiling but
    apparently including pyechonest 'breaks' the logging mechanism. I'm
    not sure why.

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 import web
+import json
 import logging
 from web import form
 from bandList import getTopArtistsFromID
 from concertList import getConcertsByID
+from setLists import getSetLists
+from getMultimediaFromTracks import getFullData
+from getUnlistenedTracks import getUnplayedTracks
 
 urls = (
     '/', 'index',
 )
-
 
 app = web.application(urls, globals())
 
@@ -42,11 +45,17 @@ class index:
             logging.info("entering getTopArtists. numBands = %s" %(str(numBands)))
             topArtists = getTopArtistsFromID(lfmID, numBands)
             logging.info("exiting getTopArtists.")
+
+            # create a ref table for MBID and band name:
+            artistByID = {}
             
+            for band in topArtists:
+                artistByID[topArtists[band]['mbid']] = band
+            
+            # look for upcoming concerts in area based on top artists
             min = '2010-10-20'
             max = '2011-10-20'
 
-            # look for upcoming concerts in area based on top artists
             logging.info("entering getConcertsByID loop for local shows")
             upConcerts = {}
             for band in topArtists:
@@ -64,15 +73,43 @@ class index:
 
             # cross-reference against all concerts in the DB from past year.
             bandSetlists = {}
-            for bands in upBands:
+            for band in upBands:
+                logging.info("getting past concerts for %s" % (band))
                 bandSetlists[band] = []
-#                cids = 
+                cids = getConcertsByID(band, None, '2009-10-20', '2010-10-20')
+                for cid in cids.keys():
+                    logging.info("searching setlist for %s" % (cid))
+                    songList = getSetLists(cid, cids[cid]['mbid'])
+                    if songList:
+                        logging.info("setlist found for %s" %(band))
+                        bandSetlists[band].extend(songList)
+            
+            unplayed_tracks = getUnplayedTracks(bandSetlists)
+            full_data = getFullData(unplayed_tracks)
 
-            return render.response(upConcerts)
+            pull_data = {}
+            
+            for items in full_data:
+                if "preview_url" in items:
+                    pull_data[items["7digital_id"]] = { 
+                        'trackname':items['trackname'],
+                        'preview_url':items['preview_url'],
+                        'release_image':items['release_image'],
+                        'artistname':items['artistname']
+                        }
+            
+                
+            
+            #print full_data
+            #f = open("pull_data.json", "w")
+            #bandList = json.dump(pull_data, f, indent=4)
+            #f.close()
+
+
+            return render.response(upConcerts, pull_data)
             
 if __name__ == "__main__": 
     logging.basicConfig(
         level=logging.DEBUG, format="%(asctime)s:%(levelname)7s: %(message)s")
     logging.info("Initializing web.py server")
     app.run()
-  
